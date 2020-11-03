46e4cbb3f7be78ae2f603e47b695f739d0745f00
MSchwartz <mschwartz@clubready.com>
2020-11-02 20:20:51 -0600

    added code

---
+++ src/main/java/org/launchcode/javawebdevtechjobsauthentication/models/dto/RegisterFormDTO.java
@@ -0,0 +1,26 @@
+package org.launchcode.javawebdevtechjobsauthentication.models.dto;
+
+import org.launchcode.javawebdevtechjobsauthentication.models.User;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+public class RegisterFormDTO extends LoginFormDTO {
+
+    private String verifyPassword;
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+    }
+
+
+}
